80ec880fd5c393ba25599ab02a83699715fa7d87
Pragati215 <dhakanepragati4@gmail.com>
2020-10-29 18:43:41 +0530

    Modified: Monthly Employee Wage is Computed using whileloop

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,14 +3,16 @@ public class EmployeeWage {
        public static final int IS_FULL_TIME = 2;
        public static final int EMP_RATE_PER_HOUR = 20;
        public static final int NUM_OF_WORKING_DAYS = 20;
+       public static final int MAX_HRS_IN_MONTH = 100;
 
       public static void main(String[] args) {
            int empHrs = 0;
-           int empWage = 0;
-           int totalEmpWage = 0;
-      for (int day = 0; day <  NUM_OF_WORKING_DAYS; day++) {
-      int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-          switch (empCheck) {
+           int totalWorkingDays = 0;
+           int totalEmpHrs = 0;
+	 while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+         totalWorkingDays++;
+         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+           switch (empCheck) {
            case IS_PART_TIME:
            System.out.println("Employee is Present");
            empHrs = 4;
@@ -23,10 +25,11 @@ public class EmployeeWage {
            System.out.println("Employee is Absent");
            empHrs = 0;
          }
-           empWage = empHrs * EMP_RATE_PER_HOUR;
-	   totalEmpWage += empWage;
-           System.out.println("Emp Wage : " + empWage);
-         }
-          System.out.println("Total Emp Wage : " + totalEmpWage);
+           totalEmpHrs += empHrs;
+           System.out.println("Day : " + totalWorkingDays + "Emp Hr: " +empHrs);
+   
+          }
+	   int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+           System.out.println("Total Emp Wage : " + totalEmpWage);
     }
 }
